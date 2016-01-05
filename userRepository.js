1d163243f52b8b4a97aea9d67d8bc9ab0c815d4c
niels <nm@9la.dk>
2016-01-05 17:55:38 +0100

    Add one more player

--- userRepository.js
+++ userRepository.js
@@ -8,10 +8,15 @@ class UserRepository {
             john: {
                 id: "36ecb365-bc76-41f7-b8ba-8ccd1d4993cc",
                 username: "john"
+            },
+            niels: {
+                id: "acbcdff3-ca47-4d6c-8df5-f9002de449a9",
+                username: "niels"
             }
         };
         this.passwords = {
-            "36ecb365-bc76-41f7-b8ba-8ccd1d4993cc": "1234"
+            "36ecb365-bc76-41f7-b8ba-8ccd1d4993cc": "1234",
+            "acbcdff3-ca47-4d6c-8df5-f9002de449a9": "1234"
         };
     }
 
